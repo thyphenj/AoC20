c2cffaf901241f0b57228c9caf8fa59f47d13d46
t-j <t-j@terence-john.co.uk>
2020-12-05 16:44:10 +0000

    05-ii completed

---
+++ 05-BinaryBoarding/Pass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace _05_BinaryBoarding
+{
+    public class Pass
+    {
+        public string Raw { set; get; }
+        public int Seat { get; set; }
+        public int Row { get; set; }
+        public int ID { get; set; }
+
+        public Pass(string str)
+        {
+            Raw = str;
+            ID = Bin2Dec(Raw);
+            Row = Bin2Dec(Raw.Substring(0, 7));
+            Seat = Bin2Dec(Raw.Substring(7, 3));
+        }
+        static private int Bin2Dec(string str)
+        {
+            int sum = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                sum = sum * 2 + (str[i] == 'F' || str[i] == 'L' ? 0 : 1);
+            }
+            return sum;
+        }
+    }
+}
---
+++ 05-BinaryBoarding/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Linq;
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace _05_BinaryBoarding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //------------------------------- Acquire raw input --------------------------------
+            string[] input = File.ReadAllLines(@"Resources/input.txt");
+
+            //------------------------------- Massage input --------------------------------
+            List<Pass> passes = new List<Pass>();
+            {
+                foreach (var line in input)
+                {
+                    passes.Add(new Pass(line));
+                }
+            }
+
+            System.Console.WriteLine("--------------------- Part 1 ---------------------------");
+            {
+                int max = 0;
+                foreach (var p in passes)
+                {
+                    if (p.ID > max)
+                        max = p.ID;
+                }
+                Console.WriteLine(max);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("--------------------- Part 2 ---------------------------");
+            {
+                int counter = 0;
+                foreach (var pass in passes.OrderBy(a => a.ID))
+                {
+                    if (pass.ID != counter)
+                    {
+                        Console.WriteLine($"{counter,4} {pass.ID - 1}");
+                        counter = pass.ID;
+                    }
+
+
+                    counter++;
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("--------------------- END ---------------------------");
+        }
+    }
+}
