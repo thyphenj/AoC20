5fc7f7cb321b3c87333aab301ef345dbd1587b95
t-j <t-j@terence-john.co.uk>
2020-12-06 11:06:22 +0000

    06-ii

---
+++ 06-CustomCustoms/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Linq;
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace _06_CustomCustoms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //------------------------------- Acquire raw input --------------------------------
+            string input = File.ReadAllText(@"Resources/input.txt");
+
+            string[] groups = input.Split("\n\n");
+
+            Console.WriteLine("------------------- Part 1 -----------------------");
+
+            int total = 0;
+            foreach (var group in groups)
+            {
+                HashSet<char> characters = new HashSet<char>();
+                foreach (var line in group.Split('\n'))
+                {
+                    foreach (char ch in line)
+                    {
+                        characters.Add(ch);
+                    }
+                }
+                total += characters.Count;
+            }
+            Console.WriteLine(total);
+            Console.WriteLine();
+
+            Console.WriteLine("------------------- Part 2 -----------------------");
+            HashSet<char> everything = new HashSet<char>();
+            for (int i = 0; i < 26; i++)
+            {
+                everything.Add(Convert.ToChar('a' + i));
+            }
+            total = 0;
+            foreach (var group in groups)
+            {
+                HashSet<char> prev = everything;
+                HashSet<char> next = new HashSet<char>();
+
+                foreach (var line in group.Split('\n'))
+                {
+                    foreach (var ch in line)
+                    {
+                        if (prev.Contains(ch))
+                        {
+                            next.Add(ch);
+                        }
+                    }
+                    prev = next;
+                    next = new HashSet<char>();
+                }
+                total += prev.Count;
+            }
+            Console.WriteLine(total);
+            Console.WriteLine();
+        }
+    }
+}
