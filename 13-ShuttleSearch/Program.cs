5fa6847162f83011791c55c5e15e771e2f662070
t-j <t-j@terence-john.co.uk>
2020-12-14 23:01:45 +0000

    REEEAALLLLLLLY late with yesterdays but here's 13-i and 13-ii with 14-i for good measure

---
+++ 13-ShuttleSearch/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _13_ShuttleSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool test = false;
+            string[] input = File.ReadAllLines(@"Resources/input.txt");
+            if (test)
+            {
+                input = new string[] { "939", "7,13,x,x,59,x,31,19" };
+            }
+            long startTime = long.Parse(input[0]);
+            List<int> freqs = new List<int>();
+            string[] work = input[1].Split(',');
+            foreach (string str in work)
+                if (str != "x")
+                    freqs.Add(int.Parse(str));
+                else
+                    freqs.Add(-1);
+
+            Console.WriteLine("13-ShuttleSearch\n-------------------------------- Part 1");
+
+            int timeStamp = freqs.Max() + 1;
+            int waitFreq = 0;
+            foreach (var f in freqs)
+            {
+                if (f > 0)
+                {
+                    int waitTime = (int)((long)f - startTime % (long)f);
+                    if (waitTime < timeStamp)
+                    {
+                        timeStamp = waitTime;
+                        waitFreq = f;
+                    }
+                }
+            }
+            Console.WriteLine($"{waitFreq * timeStamp}");
+
+            Console.WriteLine("\n-------------------------------- Part 2");
+
+            work = input[1].Split(',');
+            List<Number> numbers = new List<Number>();
+            int i = 0;
+            long N = 1;
+            foreach (string str in work)
+            {
+                Number t;
+                if (str[0] != 'x')
+                {
+                    t = new Number(str, i);
+                    numbers.Add(t);
+                    N *= t.Mod;
+                }
+                i++;
+            }
+
+            long sum = 0;
+            foreach (var num in numbers)
+            {
+                sum += num.SetYZ(N);
+            }
+            sum %= N;
+
+            Console.WriteLine(sum);
+        }
+    }
+    class Number
+    {
+        public int Mod { get; set; }
+        public int Bi { get; set; }
+        public long Ni { get; set; }
+        public int Xi { get; set; }
+        public long Product { get; set; }
+
+        public Number(string str, int i)
+        {
+            Mod = int.Parse(str);
+            Bi = Mod - i;
+        }
+        public long SetYZ(long n)
+        {
+            Ni = n / Mod;
+
+            long fac = Ni % Mod;
+            int x = 1;
+            while (fac != 1)
+            {
+                x++;
+                fac += Ni % Mod;
+                fac %= Mod;
+            }
+            Xi = x;
+            Product = Bi * Ni * Xi;
+
+            return Product;
+        }
+        public override string ToString()
+        {
+            return $"{Mod,3} - {Bi,2} {Ni} {Xi} {Product}";
+        }
+    }
+}
---
+++ 14-DockingData/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace _14_DockingData
+{
+    class Program
+    {
+        static string MASK_RAW;
+        static long MASK_LOSE;
+        static long MASK_KEEP;
+        static long MASK_MEMR;
+
+        static void Main(string[] args)
+        {
+            bool test = true;
+            string[] input = File.ReadAllLines(@"Resources/input.txt");
+
+            Console.WriteLine("14-DockingData\n-------------------------------- Part 1");
+            if (test)
+                input = new string[] {
+                    "mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X",
+                    "mem[8] = 11",
+                    "mem[7] = 101",
+                    "mem[8] = 0" };
+
+            Dictionary<long, long> memory = new Dictionary<long, long>();
+
+            foreach (var line in input)
+            {
+                if (line.Substring(0, 7) == "mask = ")
+                {
+                    MASK_RAW = line.Substring(7);
+
+                    MASK_KEEP = 0;
+                    MASK_LOSE = 0;
+                    foreach (var ch in MASK_RAW)
+                    {
+                        MASK_KEEP = MASK_KEEP * 2 + (long)(ch == '0' ? 0 : 1);
+                        MASK_LOSE = MASK_LOSE * 2 + (long)(ch == '1' ? 1 : 0);
+                    }
+                }
+                else
+                {
+                    string[] work = line.Split(" = ");
+                    long val = (long.Parse(work[1]) & MASK_KEEP) | MASK_LOSE;
+
+                    work[0] = work[0].Substring(0, work[0].Length - 1);
+                    work[0] = work[0].Substring(4);
+                    long mem = long.Parse(work[0]);
+                    if (memory.ContainsKey(mem))
+                        memory[mem] = val;
+                    else
+                        memory.Add(mem, val);
+                }
+            }
+
+            long sum = 0;
+            foreach (var m in memory)
+                sum += m.Value;
+            Console.WriteLine(sum);
+
+            Console.WriteLine("\n-------------------------------- Part 2");
+            if (test)
+            {
+                input = new string[] {
+                    "mask = 000000000000000000000000000000X1001X",
+                    "mem[42] = 100",
+                    "mask = 00000000000000000000000000000000X0XX",
+                    "mem[26] = 1" };
+            }
+            memory = new Dictionary<long, long>();
+            Mask mask = new Mask("");
+
+            foreach (var line in input)
+            {
+                if (line.Substring(0, 7) == "mask = ")
+                {
+                    mask = new Mask(line.Substring(7));
+                }
+                else
+                {
+                    string[] work = line.Split(" = ");
+
+                    long val = long.Parse(work[1]);
+                    long mem = long.Parse(work[0].Substring(4, work[0].Length - 5));
+                    mem = mask.Apply(mem);
+
+                    if (memory.ContainsKey(mem))
+                        memory[mem] = val;
+                    else
+                        memory.Add(mem, val);
+                }
+            }
+
+            sum = 0;
+            foreach (var m in memory)
+                sum += m.Value;
+            Console.WriteLine(sum);
+
+        }
+        static long ApplyBitMask(long val)
+        {
+            return ((val & MASK_KEEP) | MASK_LOSE);
+        }
+
+    }
+}
