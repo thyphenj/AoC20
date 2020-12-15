a866481aed995f454cf16a738bbc1c9130587b1f
t-j <t-j@terence-john.co.uk>
2020-12-15 14:59:47 +0000

    14-ii

--- 14-DockingData/Mask.cs
+++ 14-DockingData/Mask.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace _14_DockingData
 {
     public class Mask
@@ -7,6 +9,8 @@ namespace _14_DockingData
         public long MASK_KEP;
         public long MASK_SET;
         public long MASK_FLT;
+
+        private List<long> toggler;
 
         public Mask(string line)
         {
@@ -17,31 +21,49 @@ namespace _14_DockingData
                 MASK_SET = 0;   //OR  with 1 (for mask = 1)
                 MASK_FLT = 0;
 
+                toggler = new List<long>();
+
+                int i = 0;
                 foreach (var ch in MASK_RAW)
                 {
                     MASK_KEP = MASK_KEP * 2 + (long)(ch == '0' ? 1 : 0);
                     MASK_SET = MASK_SET * 2 + (long)(ch == '1' ? 1 : 0);
                     MASK_FLT = MASK_FLT * 2 + (long)(ch == 'X' ? 1 : 0);
+                    if (ch == 'X')
+                    {
+                        toggler.Add((long)Math.Pow(2, 35 - i));
+                    }
+                    i++;
                 }
-                Console.WriteLine("--------------Set MASK");
-                Console.WriteLine(MASK_RAW);
-                Console.WriteLine(Convert.ToString(MASK_KEP, 2).PadLeft(36, '0'));
-                Console.WriteLine(Convert.ToString(MASK_SET, 2).PadLeft(36, '0'));
-                Console.WriteLine(Convert.ToString(MASK_FLT, 2).PadLeft(36, '0'));
-                Console.WriteLine("--------------");
             }
         }
 
-        public long Apply(long val)
+        public List<long> Apply(long val)
         {
-            long work = val;
-            Console.WriteLine(Convert.ToString(work, 2).PadLeft(36, '0'));
-            work = work & MASK_KEP;
-            Console.WriteLine(Convert.ToString(work, 2).PadLeft(36, '0'));
-            work = work | MASK_SET;
-            Console.WriteLine(Convert.ToString(work, 2).PadLeft(36, '0'));
-            Console.WriteLine("--------------");
-            return work;
+            List<long> retval = new List<long>();
+
+            long baseVal = val;
+
+            baseVal &= MASK_KEP;
+            baseVal |= MASK_SET;
+
+            retval.Add(baseVal);
+
+            int bitmapLen = (int)Math.Pow(2, toggler.Count);
+
+            for (uint bitmap = 0; bitmap < bitmapLen; bitmap++)
+            {
+                long nextVal = baseVal;
+
+                int bit = 1;
+                for (int i = 0; i < toggler.Count; i++)
+                {
+                    if ((bitmap & bit) == bit) nextVal |= toggler[i];
+                    bit <<= 1;
+                }
+                retval.Add(nextVal);
+            }
+            return retval;
         }
 
     }
--- 14-DockingData/Program.cs
+++ 14-DockingData/Program.cs
@@ -9,95 +9,97 @@ namespace _14_DockingData
         static string MASK_RAW;
         static long MASK_LOSE;
         static long MASK_KEEP;
-        static long MASK_MEMR;
 
         static void Main(string[] args)
         {
-            bool test = true;
+            bool test = false;
             string[] input = File.ReadAllLines(@"Resources/input.txt");
 
             Console.WriteLine("14-DockingData\n-------------------------------- Part 1");
-            if (test)
-                input = new string[] {
+            {
+                if (test)
+                    input = new string[] {
                     "mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X",
                     "mem[8] = 11",
                     "mem[7] = 101",
                     "mem[8] = 0" };
 
-            Dictionary<long, long> memory = new Dictionary<long, long>();
+                Dictionary<long, long> memory = new Dictionary<long, long>();
 
-            foreach (var line in input)
-            {
-                if (line.Substring(0, 7) == "mask = ")
+                foreach (var line in input)
                 {
-                    MASK_RAW = line.Substring(7);
-
-                    MASK_KEEP = 0;
-                    MASK_LOSE = 0;
-                    foreach (var ch in MASK_RAW)
+                    if (line.Substring(0, 7) == "mask = ")
                     {
-                        MASK_KEEP = MASK_KEEP * 2 + (long)(ch == '0' ? 0 : 1);
-                        MASK_LOSE = MASK_LOSE * 2 + (long)(ch == '1' ? 1 : 0);
+                        MASK_RAW = line.Substring(7);
+
+                        MASK_KEEP = 0;
+                        MASK_LOSE = 0;
+                        foreach (var ch in MASK_RAW)
+                        {
+                            MASK_KEEP = MASK_KEEP * 2 + (long)(ch == '0' ? 0 : 1);
+                            MASK_LOSE = MASK_LOSE * 2 + (long)(ch == '1' ? 1 : 0);
+                        }
+                    }
+                    else
+                    {
+                        string[] work = line.Split(" = ");
+                        long val = (long.Parse(work[1]) & MASK_KEEP) | MASK_LOSE;
+
+                        work[0] = work[0].Substring(0, work[0].Length - 1);
+                        work[0] = work[0].Substring(4);
+                        long mem = long.Parse(work[0]);
+                        if (memory.ContainsKey(mem))
+                            memory[mem] = val;
+                        else
+                            memory.Add(mem, val);
                     }
                 }
-                else
-                {
-                    string[] work = line.Split(" = ");
-                    long val = (long.Parse(work[1]) & MASK_KEEP) | MASK_LOSE;
 
-                    work[0] = work[0].Substring(0, work[0].Length - 1);
-                    work[0] = work[0].Substring(4);
-                    long mem = long.Parse(work[0]);
-                    if (memory.ContainsKey(mem))
-                        memory[mem] = val;
-                    else
-                        memory.Add(mem, val);
-                }
+                long sum = 0;
+                foreach (var m in memory)
+                    sum += m.Value;
+                Console.WriteLine(sum);
             }
-
-            long sum = 0;
-            foreach (var m in memory)
-                sum += m.Value;
-            Console.WriteLine(sum);
-
             Console.WriteLine("\n-------------------------------- Part 2");
-            if (test)
             {
-                input = new string[] {
+                if (test)
+                    input = new string[] {
                     "mask = 000000000000000000000000000000X1001X",
                     "mem[42] = 100",
                     "mask = 00000000000000000000000000000000X0XX",
                     "mem[26] = 1" };
-            }
-            memory = new Dictionary<long, long>();
-            Mask mask = new Mask("");
 
-            foreach (var line in input)
-            {
-                if (line.Substring(0, 7) == "mask = ")
+                Dictionary<long, long> memory = new Dictionary<long, long>();
+                Mask mask = new Mask("");
+
+                foreach (var line in input)
                 {
-                    mask = new Mask(line.Substring(7));
-                }
-                else
-                {
-                    string[] work = line.Split(" = ");
-
-                    long val = long.Parse(work[1]);
-                    long mem = long.Parse(work[0].Substring(4, work[0].Length - 5));
-                    mem = mask.Apply(mem);
-
-                    if (memory.ContainsKey(mem))
-                        memory[mem] = val;
+                    if (line.Substring(0, 7) == "mask = ")
+                    {
+                        mask = new Mask(line.Substring(7));
+                    }
                     else
-                        memory.Add(mem, val);
+                    {
+                        string[] work = line.Split(" = ");
+
+                        long val = long.Parse(work[1]);
+                        long mem = long.Parse(work[0].Substring(4, work[0].Length - 5));
+
+                        foreach (var m in mask.Apply(mem))
+                        {
+                            if (memory.ContainsKey(m))
+                                memory[m] = val;
+                            else
+                                memory.Add(m, val);
+                        }
+                    }
                 }
+
+                long sum = 0;
+                foreach (var m in memory)
+                    sum += m.Value;
+                Console.WriteLine(sum);
             }
-
-            sum = 0;
-            foreach (var m in memory)
-                sum += m.Value;
-            Console.WriteLine(sum);
-
         }
         static long ApplyBitMask(long val)
         {
